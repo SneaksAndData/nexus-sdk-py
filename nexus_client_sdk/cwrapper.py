9eb3c02d0109f8e8594854cab82a8b94d0c28e92
George Zubrienko <zubrienkog@gmail.com>
2025-06-19 11:52:15 +0200

    Rename package to fit into PyPi (#13)

--- nexus_client_sdk/cwrapper.py
+++ nexus_client_sdk/cwrapper.py
@@ -5,5 +5,5 @@ import os
 import pathlib
 from ctypes import cdll
 
-_LIB_DEFAULT_LOCATION = os.path.join(pathlib.Path(__file__).parent.resolve(), ".extensions", "nexus_sdk.so")
+_LIB_DEFAULT_LOCATION = os.path.join(pathlib.Path(__file__).parent.resolve(), ".extensions", "nexus_client_sdk.so")
 CLIB = cdll.LoadLibrary(_LIB_DEFAULT_LOCATION)
