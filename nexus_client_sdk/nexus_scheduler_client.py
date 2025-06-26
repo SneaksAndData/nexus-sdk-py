a9a2b9878281815079c9d119b8403746075532ad
George Zubrienko <gzu@ecco.com>
2025-06-26 13:44:40 +0200

    wip

--- nexus_client_sdk/nexus_scheduler_client.py
+++ nexus_client_sdk/nexus_scheduler_client.py
@@ -1,7 +1,8 @@
 """Scheduler"""
-
+import asyncio
 import ctypes
 import json
+from array import array
 
 from typing import final, Callable, Self, Iterator, Any
 
@@ -43,6 +44,14 @@ class NexusSchedulerClient:
         self._create_run = CLIB.CreateRun
         self._create_run.restype = SdkAlgorithmRun
 
+        self._await_run = CLIB.AwaitRun
+        self._await_run.restype = SdkRunResult
+
+        self._await_tagged_runs = CLIB.AwaitTaggedRuns
+        self._await_tagged_runs.restype = ctypes.POINTER(SdkRunResult)
+
+        self._free_results_array = CLIB.FreeRunResultsPointer
+
     def __del__(self):
         CLIB.FreeClient(self._client)
 
@@ -57,18 +66,7 @@ class NexusSchedulerClient:
             self._current_token = self._token_provider() if self._token_provider is not None else AccessToken.empty()
             self._update_token(bytes(self._current_token.value, encoding="utf-8"))
 
-    def get_run_results(self, tag: str) -> Iterator[RunResult]:
-        """
-         Retrieves run results for a given tag.
-        :param tag: Client-side assigned run tag.
-        :return: Run result collection.
-        """
-        self._init_client()
-        results: Iterator[SdkRunResult] = self._get_run_results(bytes(tag, encoding="utf-8"))
-        if not results:
-            raise RuntimeError(
-                "Unmapped SDK error: Go client failed to return coherent result. This is a bug and must be reported to the maintainer team."
-            )
+    def _iterate_results(self, results: Iterator[SdkRunResult]) -> Iterator[RunResult]:
         for result in results:
             maybe_result = RunResult.from_sdk_result(result)
             if maybe_result is None:
@@ -81,6 +79,23 @@ class NexusSchedulerClient:
                     break
                 case _:
                     raise maybe_result.error()
+
+        self._free_results_array(results)
+
+    def get_run_results(self, tag: str, algorithm: str | None = None) -> Iterator[RunResult]:
+        """
+         Retrieves run results for a given tag.
+        :param tag: Client-side assigned run tag.
+        :param algorithm: Optional algorithm to filter returned results by.
+        :return: Run result collection.
+        """
+        self._init_client()
+        results: Iterator[SdkRunResult] = self._get_run_results(bytes(tag, encoding="utf-8"), bytes(algorithm, encoding="utf-8") if algorithm else None)
+        if not results:
+            raise RuntimeError(
+                "Unmapped SDK error: Go client failed to return coherent result. This is a bug and must be reported to the maintainer team."
+            )
+        return self._iterate_results(results)
 
     def create_run(
         self,
@@ -106,7 +121,7 @@ class NexusSchedulerClient:
             bytes(algorithm_name, encoding="utf-8"),
             bytes(json.dumps(algorithm_parameters), encoding="utf-8"),
             custom_configuration.as_pointer() if custom_configuration else None,
-            parent_request.as_pointer() if parent_request is not None else None,
+            parent_request.as_pointer() if parent_request else None,
             bytes(payload_valid_for, encoding="utf-8"),
             bytes(tag, encoding="utf-8") if tag else None,
         )
@@ -118,6 +133,63 @@ class NexusSchedulerClient:
                 return converted.request_id
             case _:
                 raise converted.error()
+
+    def await_run(self, request_id: str, algorithm: str, poll_interval_seconds = 5) -> RunResult:
+        """
+          Awaits result for a given run for a given algorithm.
+        :param request_id: Run request ID.
+        :param algorithm: Algorithm name.
+        :param poll_interval_seconds: Time between status checks
+        :return:
+        """
+        self._init_client()
+        maybe_result = self._await_run(
+            bytes(request_id, encoding="utf-8"),
+            bytes(algorithm, encoding="utf-8"),
+            ctypes.c_int32(poll_interval_seconds),
+        )
+
+        converted = RunResult.from_sdk_result(maybe_result)
+
+        match converted.error():
+            case None:
+                return converted
+            case _:
+                raise converted.error()
+
+
+    def await_tagged(self, tags: list[str], algorithm: str | None, poll_interval_seconds = 5, report_progress = True):
+        """
+         Awaits all runs with matching tags.
+        :param tags: Tags to use when filtering runs
+        :param algorithm: Optional algorithm name to filter tagged runs by. Only set this if client might use the same tag for multiple algorithms.
+        :param poll_interval_seconds: Time between status checks
+        :param report_progress: Whether to report overall progress.
+        :return:
+        """
+        def _await_tagged():
+            return self._iterate_results(self._await_tagged_runs(tags_array_ptr, bytes(algorithm, encoding="utf-8") if algorithm else None, ctypes.c_int32(poll_interval_seconds)))
+
+        async def _await_tagged_async():
+            return _await_tagged()
+
+        self._init_client()
+        tags_array = array("u", tags)
+        tags_array_ptr = (ctypes.c_char_p * len(tags_array)).from_buffer(tags_array)
+        if not report_progress:
+            return _await_tagged()
+
+        loop = asyncio.get_event_loop()
+        task = loop.create_task(_await_tagged_async())
+        while True:
+            if task.done() and task.exception() is None:
+                return task.result()
+            if task.exception() is not None:
+                raise task.exception()
+
+            asyncio.sleep(1)
+            # check progress and report if there is any
+
 
     @classmethod
     def create(cls, url: str, token_provider: Callable[[], AccessToken] | None = None) -> Self:
