7811ab2cbe4adc6b2f5a1a0d0f7bedddd0ba6908
George Zubrienko <zubrienkog@gmail.com>
2025-07-02 11:30:36 +0200

    Receiver API (#21)

--- nexus_client_sdk/models/scheduler.py
+++ nexus_client_sdk/models/scheduler.py
@@ -4,12 +4,7 @@ from dataclasses import dataclass
 from typing import final, Self
 
 from nexus_client_sdk.cwrapper import CLIB
-from nexus_client_sdk.models.client_errors.go_http_errors import (
-    SdkError,
-    UnauthorizedError,
-    BadRequestError,
-    NotFoundError,
-)
+from nexus_client_sdk.models.common import PySdkType
 
 
 @final
@@ -30,32 +25,6 @@ class SdkRunResult(ctypes.Structure):
 
     def __del__(self):
         CLIB.FreeRunResult(self)
-
-
-@dataclass
-class PySdkType:
-    """
-    Base class for Python model type wrappers
-    """
-
-    client_error_type: str | None
-    client_error_message: str | None
-
-    def error(self) -> RuntimeError | None:
-        """
-         Parse Go client error into a corresponding Python error.
-        :return:
-        """
-        match self.client_error_type:
-            case "*models.SdkErr":
-                return SdkError(self.client_error_message)
-            case "*models.UnauthorizedError":
-                return UnauthorizedError(self.client_error_message)
-            case "*models.BadRequestError":
-                return BadRequestError(self.client_error_message)
-            case "*models.NotFoundError":
-                return NotFoundError(self.client_error_message)
-        return None
 
 
 @dataclass
