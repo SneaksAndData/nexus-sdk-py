3623f90c2cd77516f5b0b0002129ba17e858450b
George Zubrienko <zubrienkog@gmail.com>
2025-08-05 13:52:40 +0200

    Update license to better showcase equal OSS IP rights of collaborators outside ECCO (#29)

--- nexus_client_sdk/nexus/input/payload_reader.py
+++ nexus_client_sdk/nexus/input/payload_reader.py
@@ -2,7 +2,7 @@
  Code infrastructure for manipulating payload received from Nexus
 """
 
-#  Copyright (c) 2023-2024. ECCO Sneaks & Data
+#  Copyright (c) 2023-2026. ECCO Data & AI and other project contributors.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
