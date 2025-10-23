0f4f96ad61f9b3e061de571fca1e2406f4af0cd0
George Zubrienko <gzu@ecco.com>
2025-10-23 15:10:24 +0200

    Move hanging tests to a separate module

---
+++ tests/test_create_and_await.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+import pytest
+from cassandra.cluster import Session
+
+from nexus_client_sdk.nexus.async_extensions.async_retry.async_retry_policy import (
+    NexusSchedulerRuntimeError,
+)
+from nexus_client_sdk.nexus.async_extensions.nexus_scheduler_async_client import NexusSchedulerAsyncClient
+
+runtime_config_stub = (
+    open(Path(__file__).parent / "mock_data" / "applied_configuration.json", encoding="utf-8").read().replace("\n", " ")
+)
+
+
+@pytest.mark.asyncio
+async def test_create_and_await(async_scheduler: NexusSchedulerAsyncClient):
+    result = await async_scheduler.create_and_await(algorithm_parameters={}, algorithm_name="hello-world")
+
+    assert async_scheduler._sync_client.is_finished(result) and not async_scheduler._sync_client.has_succeeded(result)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("propagate", [True, False])
+async def test_custom_error(propagate: bool, async_scheduler: NexusSchedulerAsyncClient, cql_session: Session):
+    if propagate:
+        with pytest.raises(NexusSchedulerRuntimeError):
+            _ = await async_scheduler.create_and_await(
+                algorithm_parameters={},
+                algorithm_name="hello-world",
+                propagate_error=propagate,
+                post_create_callback=lambda run_id: cql_session.execute(
+                    f"INSERT INTO nexus.checkpoints (algorithm, id, lifecycle_stage, payload_uri, applied_configuration, configuration_overrides, parent) VALUES ('hello-world', '{run_id}', 'SCHEDULING_FAILED', '', '{runtime_config_stub}', '{{}}', '{{}}')"
+                ),
+            )
+    else:
+        assert (
+            await async_scheduler.create_and_await(
+                algorithm_parameters={},
+                algorithm_name="hello-world",
+                propagate_error=propagate,
+                post_create_callback=lambda run_id: cql_session.execute(
+                    f"INSERT INTO nexus.checkpoints (algorithm, id, lifecycle_stage, payload_uri, applied_configuration, configuration_overrides, parent) VALUES ('hello-world', '{run_id}', 'SCHEDULING_FAILED', '', '{runtime_config_stub}', '{{}}', '{{}}')"
+                ),
+            )
+            is None
+        )
