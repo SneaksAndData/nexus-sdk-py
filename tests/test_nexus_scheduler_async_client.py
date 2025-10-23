c68bf9a3ef95500d1d2a8ecfc15529a103ba17ad
George Zubrienko <gzu@ecco.com>
2025-10-23 11:17:42 +0200

    try with 1 test only

--- tests/test_nexus_scheduler_async_client.py
+++ tests/test_nexus_scheduler_async_client.py
@@ -20,63 +20,63 @@ async def test_create_run_propagates(async_scheduler: NexusSchedulerAsyncClient)
     with pytest.raises(BadRequestError):
         _ = await async_scheduler.create_run(algorithm_parameters={}, algorithm_name="non-existing")
 
-
-@pytest.mark.asyncio
-@pytest.mark.xdist_group(name="async_scheduler")
-async def test_create_run_retries(broken_async_scheduler: NexusSchedulerAsyncClient):
-    with pytest.raises(NexusSchedulerRuntimeError):
-        _ = await broken_async_scheduler.create_run(algorithm_parameters={}, algorithm_name="hello-world")
-
-
-@pytest.mark.asyncio
-@pytest.mark.xdist_group(name="async_scheduler")
-async def test_await_run_retries(broken_async_scheduler: NexusSchedulerAsyncClient):
-    with pytest.raises(NexusSchedulerRuntimeError):
-        _ = await broken_async_scheduler.await_run(
-            request_id="test",
-            algorithm="test",
-        )
-
-
-@pytest.mark.asyncio
-@pytest.mark.xdist_group(name="async_scheduler")
-async def test_create_and_await(async_scheduler: NexusSchedulerAsyncClient):
-    result = await async_scheduler.create_and_await(algorithm_parameters={}, algorithm_name="hello-world")
-
-    assert async_scheduler._sync_client.is_finished(result) and not async_scheduler._sync_client.has_succeeded(result)
-
-
-@pytest.mark.asyncio
-@pytest.mark.xdist_group(name="async_scheduler")
-async def test_create_and_await(async_scheduler: NexusSchedulerAsyncClient):
-    result = await async_scheduler.create_and_await(algorithm_parameters={}, algorithm_name="hello-world")
-
-    assert async_scheduler._sync_client.is_finished(result) and not async_scheduler._sync_client.has_succeeded(result)
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize("propagate", [True, False])
-@pytest.mark.xdist_group(name="async_scheduler")
-async def test_custom_error(propagate: bool, async_scheduler: NexusSchedulerAsyncClient, cql_session: Session):
-    if propagate:
-        with pytest.raises(NexusSchedulerRuntimeError):
-            _ = await async_scheduler.create_and_await(
-                algorithm_parameters={},
-                algorithm_name="hello-world",
-                propagate_error=propagate,
-                post_create_callback=lambda run_id: cql_session.execute(
-                    f"INSERT INTO nexus.checkpoints (algorithm, id, lifecycle_stage, payload_uri, applied_configuration, configuration_overrides, parent) VALUES ('hello-world', '{run_id}', 'SCHEDULING_FAILED', '', '{runtime_config_stub}', '{{}}', '{{}}')"
-                ),
-            )
-    else:
-        assert (
-            await async_scheduler.create_and_await(
-                algorithm_parameters={},
-                algorithm_name="hello-world",
-                propagate_error=propagate,
-                post_create_callback=lambda run_id: cql_session.execute(
-                    f"INSERT INTO nexus.checkpoints (algorithm, id, lifecycle_stage, payload_uri, applied_configuration, configuration_overrides, parent) VALUES ('hello-world', '{run_id}', 'SCHEDULING_FAILED', '', '{runtime_config_stub}', '{{}}', '{{}}')"
-                ),
-            )
-            is None
-        )
+#
+# @pytest.mark.asyncio
+# @pytest.mark.xdist_group(name="async_scheduler")
+# async def test_create_run_retries(broken_async_scheduler: NexusSchedulerAsyncClient):
+#     with pytest.raises(NexusSchedulerRuntimeError):
+#         _ = await broken_async_scheduler.create_run(algorithm_parameters={}, algorithm_name="hello-world")
+#
+#
+# @pytest.mark.asyncio
+# @pytest.mark.xdist_group(name="async_scheduler")
+# async def test_await_run_retries(broken_async_scheduler: NexusSchedulerAsyncClient):
+#     with pytest.raises(NexusSchedulerRuntimeError):
+#         _ = await broken_async_scheduler.await_run(
+#             request_id="test",
+#             algorithm="test",
+#         )
+#
+#
+# @pytest.mark.asyncio
+# @pytest.mark.xdist_group(name="async_scheduler")
+# async def test_create_and_await(async_scheduler: NexusSchedulerAsyncClient):
+#     result = await async_scheduler.create_and_await(algorithm_parameters={}, algorithm_name="hello-world")
+#
+#     assert async_scheduler._sync_client.is_finished(result) and not async_scheduler._sync_client.has_succeeded(result)
+#
+#
+# @pytest.mark.asyncio
+# @pytest.mark.xdist_group(name="async_scheduler")
+# async def test_create_and_await(async_scheduler: NexusSchedulerAsyncClient):
+#     result = await async_scheduler.create_and_await(algorithm_parameters={}, algorithm_name="hello-world")
+#
+#     assert async_scheduler._sync_client.is_finished(result) and not async_scheduler._sync_client.has_succeeded(result)
+#
+#
+# @pytest.mark.asyncio
+# @pytest.mark.parametrize("propagate", [True, False])
+# @pytest.mark.xdist_group(name="async_scheduler")
+# async def test_custom_error(propagate: bool, async_scheduler: NexusSchedulerAsyncClient, cql_session: Session):
+#     if propagate:
+#         with pytest.raises(NexusSchedulerRuntimeError):
+#             _ = await async_scheduler.create_and_await(
+#                 algorithm_parameters={},
+#                 algorithm_name="hello-world",
+#                 propagate_error=propagate,
+#                 post_create_callback=lambda run_id: cql_session.execute(
+#                     f"INSERT INTO nexus.checkpoints (algorithm, id, lifecycle_stage, payload_uri, applied_configuration, configuration_overrides, parent) VALUES ('hello-world', '{run_id}', 'SCHEDULING_FAILED', '', '{runtime_config_stub}', '{{}}', '{{}}')"
+#                 ),
+#             )
+#     else:
+#         assert (
+#             await async_scheduler.create_and_await(
+#                 algorithm_parameters={},
+#                 algorithm_name="hello-world",
+#                 propagate_error=propagate,
+#                 post_create_callback=lambda run_id: cql_session.execute(
+#                     f"INSERT INTO nexus.checkpoints (algorithm, id, lifecycle_stage, payload_uri, applied_configuration, configuration_overrides, parent) VALUES ('hello-world', '{run_id}', 'SCHEDULING_FAILED', '', '{runtime_config_stub}', '{{}}', '{{}}')"
+#                 ),
+#             )
+#             is None
+#         )
