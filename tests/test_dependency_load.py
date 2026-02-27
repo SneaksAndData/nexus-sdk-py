ad0e10dae2535a1c4796189b62782a95c6fc1998
George Zubrienko <gzu@ecco.com>
2026-02-27 10:25:24 +0100

    Add test for DD configuration load

---
+++ tests/test_dependency_load.py
@@ -0,0 +1,14 @@
+from nexus_client_sdk.nexus.configurations.runtime_configuration import NEXUS_FRAMEWORK_CONFIGURATION
+
+
+def test_datadog_logging_init() -> None:
+    NEXUS_FRAMEWORK_CONFIGURATION.load()
+    assert (
+        NEXUS_FRAMEWORK_CONFIGURATION.default.logging.datadog.enabled == 0
+        and NEXUS_FRAMEWORK_CONFIGURATION.default.logging.datadog.debug == "False"
+        and NEXUS_FRAMEWORK_CONFIGURATION.default.logging.datadog.fixed_tags == {}
+        and NEXUS_FRAMEWORK_CONFIGURATION.default.logging.datadog.buffer_size == 1
+        and NEXUS_FRAMEWORK_CONFIGURATION.default.logging.datadog.max_flush_retry_time == 30
+        and NEXUS_FRAMEWORK_CONFIGURATION.default.logging.datadog.ignore_flush_failure == "True"
+        and NEXUS_FRAMEWORK_CONFIGURATION.default.logging.datadog.attach_interrupt_handlers == "True"
+    )
