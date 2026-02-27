e2fa1655e68c646416a453a4415c75657d5bff83
George Zubrienko <gzu@ecco.com>
2026-02-27 10:33:37 +0100

    Include env variable override

--- tests/test_dependency_load.py
+++ tests/test_dependency_load.py
@@ -1,6 +1,7 @@
 from nexus_client_sdk.nexus.configurations.runtime_configuration import NEXUS_FRAMEWORK_CONFIGURATION
 
 
+# NEXUS__LOGGING__DATADOG__IGNORE_FLUSH_FAILURE overrides to False
 def test_datadog_logging_init() -> None:
     NEXUS_FRAMEWORK_CONFIGURATION.load()
     assert (
@@ -9,6 +10,6 @@ def test_datadog_logging_init() -> None:
         and NEXUS_FRAMEWORK_CONFIGURATION.default.logging.datadog.fixed_tags == {}
         and NEXUS_FRAMEWORK_CONFIGURATION.default.logging.datadog.buffer_size == 1
         and NEXUS_FRAMEWORK_CONFIGURATION.default.logging.datadog.max_flush_retry_time == 30
-        and NEXUS_FRAMEWORK_CONFIGURATION.default.logging.datadog.ignore_flush_failure == "True"
+        and NEXUS_FRAMEWORK_CONFIGURATION.default.logging.datadog.ignore_flush_failure == "False"
         and NEXUS_FRAMEWORK_CONFIGURATION.default.logging.datadog.attach_interrupt_handlers == "True"
     )
