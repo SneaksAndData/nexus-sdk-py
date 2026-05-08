83efb12bd36e44400ce8058c7051ad8d29a11fcb
HFJ <henrikfoss_21@hotmail.com>
2026-05-08 14:22:27 +0200

    Small bugfix for payload telemetry

--- nexus_client_sdk/nexus/telemetry/payload_recorder.py
+++ nexus_client_sdk/nexus/telemetry/payload_recorder.py
@@ -39,7 +39,7 @@ class PayloadTelemetry(UserTelemetryRecorder[str, AlgorithmResult]):
                 [
                     DataFrame(
                         {
-                            "payload": algorithm_result.result()["data"],
+                            "payload": algorithm_result.result()["content"],
                             "request_id": run_id,
                             "recorded_at": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
                             "is_valid": True,
@@ -75,11 +75,11 @@ class FailedPayloadRecorder(UserTelemetryRecorder[str, AlgorithmResult]):
                 [
                     DataFrame(
                         {
-                            "payload": algorithm_result.result()["data"],
+                            "payload": algorithm_result.result()["content"],
                             "request_id": run_id,
                             "recorded_at": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
                             "is_valid": False,
-                        }
+
                     )
                 ]
             )
