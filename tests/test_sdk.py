e770a7181d954313953f52a24e111cbbd7626c2b
MAPET <marcusforby@gmail.com>
2026-08-06 15:07:18 +0200

    Fix test

--- tests/test_sdk.py
+++ tests/test_sdk.py
@@ -44,18 +44,17 @@ def _find_telemetry_objects(request_id: str) -> tuple[list[str], list[str]]:
         aws_secret_access_key=os.environ["PROTEUS__AWS_SECRET_ACCESS_KEY"],
     )
     input_prefix = "telemetry/telemetry_group=inputs/"
-    user_prefix = (
-        "telemetry/telemetry_group=user/recorder_class=test_user_analytics_telemetry/"
-        f"analysis=test-recording/{request_id}_"
-    )
+    user_prefix = "telemetry/telemetry_group=user"
 
     input_objects = [
         item["Key"]
         for item in s3_client.list_objects_v2(Bucket="nexus-sdk-tests", Prefix=input_prefix).get("Contents", [])
-        if item["Key"].endswith(f"/{request_id}")
+        if request_id in item["Key"]
     ]
     user_objects = [
-        item["Key"] for item in s3_client.list_objects_v2(Bucket="nexus-sdk-tests", Prefix=user_prefix).get("Contents", [])
+        item["Key"]
+        for item in s3_client.list_objects_v2(Bucket="nexus-sdk-tests", Prefix=user_prefix).get("Contents", [])
+        if request_id in item["Key"]
     ]
 
     return input_objects, user_objects
@@ -83,7 +82,8 @@ async def test_sdk_run(
     )  # expect 1 run of each: XYSAMPLE, ZSAMPLE, ZPROCESSOR, ZZPROCESSOR, XYPROCESSOR
 
     input_telemetry_objects, user_telemetry_objects = _find_telemetry_objects(test_args.request_id)
-    assert input_telemetry_objects and len(user_telemetry_objects) == 1
+    assert len(input_telemetry_objects) == 3  # 3 processors injected into algorithm
+    assert len(user_telemetry_objects) == 2  # 1 user telemetry + 1 payload telemetry
 
 
 @pytest.mark.asyncio(loop_scope="package")
