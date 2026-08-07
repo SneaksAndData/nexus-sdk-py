cfbe7ed1dc74bad41955dcb3edb3fa54d33c722c
MAPET <marcusforby@gmail.com>
2026-08-07 14:05:46 +0200

    Update tests

--- tests/algorithms/minimalistic/test_execution_tree.py
+++ tests/algorithms/minimalistic/test_execution_tree.py
@@ -1,21 +1,21 @@
 from nexus_client_sdk.nexus.execution.trees import get_tree
-from tests.algorithms.minimalistic.sample_main import TestAlgorithm
+from tests.algorithms.minimalistic.sample_main import TestMinimalisticAlgorithm
 
 
 def test_tree_generation():
-    tree = get_tree(TestAlgorithm)
-    assert len(tree.root_node.children) == 3 and tree.root_node.class_name == TestAlgorithm.__name__
+    tree = get_tree(TestMinimalisticAlgorithm)
+    assert len(tree.root_node.children) == 3 and tree.root_node.class_name == TestMinimalisticAlgorithm.__name__
 
 
 def test_tree_serialization():
-    mermaid_tree = get_tree(TestAlgorithm).serialize(sort_nodes=True)
+    mermaid_tree = get_tree(TestMinimalisticAlgorithm).serialize(sort_nodes=True)
 
     assert (
         mermaid_tree
         == """graph TB
-TESTALGORITHM["TestAlgorithm"] --> XYPROCESSOR["XYProcessor"]
-TESTALGORITHM["TestAlgorithm"] --> ZPROCESSOR["ZProcessor"]
-TESTALGORITHM["TestAlgorithm"] --> ZZPROCESSOR["ZZProcessor"]
+TESTMINIMALISTICALGORITHM["TestMinimalisticAlgorithm"] --> XYPROCESSOR["XYProcessor"]
+TESTMINIMALISTICALGORITHM["TestMinimalisticAlgorithm"] --> ZPROCESSOR["ZProcessor"]
+TESTMINIMALISTICALGORITHM["TestMinimalisticAlgorithm"] --> ZZPROCESSOR["ZZProcessor"]
 XYPROCESSOR["XYProcessor"] --> XYSAMPLEREADER["XYSampleReader"]
 ZPROCESSOR["ZProcessor"] --> ZSAMPLEREADER["ZSampleReader"]
 ZZPROCESSOR["ZZProcessor"] --> ZPROCESSOR["ZProcessor"]"""
--- tests/algorithms/minimalistic/test_sdk_minimalistic.py
+++ tests/algorithms/minimalistic/test_sdk_minimalistic.py
@@ -23,7 +23,7 @@ from tests.algorithms.shared import (
     TestEnum,
     NegativeZError,
 )
-from tests.algorithms.minimalistic.sample_main import main as sample_algorithm_main
+from tests.algorithms.shared import main as sample_algorithm_main
 
 os.environ["PROTEUS__AWS_REGION"] = "us-east-1"
 os.environ["PROTEUS__AWS_ENDPOINT"] = "http://localhost:9000"
@@ -41,11 +41,13 @@ def set_config_extension_path_override(monkeypatch):
 def payloads(
     compress: bool = False,
 ) -> list[tuple[str, str]]:
-    return payloads_for_algorithm("tests.algorithms.minimalistic.sample_main.TestAlgorithm", compress=compress)
+    return payloads_for_algorithm(
+        "tests.algorithms.minimalistic.sample_main.TestMinimalisticAlgorithm", compress=compress
+    )
 
 
 def negative_z_payload(
-    algorithm_class: str = "tests.algorithms.minimalistic.sample_main.TestAlgorithm",
+    algorithm_class: str = "tests.algorithms.minimalistic.sample_main.TestMinimalisticAlgorithm",
 ) -> tuple[str, str]:
     upload_path = S3Path(bucket="nexus", path="units")
 
