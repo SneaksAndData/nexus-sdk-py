fc7a63e4d9d8d1d27c5a1bc6c5889042e1fa8be4
George Zubrienko <gzu@ecco.com>
2026-08-25 13:23:27 +0200

    Fixes

---
+++ nexus_client_sdk/nexus/abstractions/qes_factory.py
@@ -0,0 +1,48 @@
+from typing import final, Self
+
+from adapta.process_communication import DataSocket
+from adapta.storage.query_enabled_store import QueryEnabledStore
+from adapta.storage.query_enabled_store._functions import suggest_store
+
+
+@final
+class QueryEnabledStoreCollection:
+    """
+    A container for all QES instances defined in NEXUS_FRAMEWORK_CONFIGURATION via respective connection strings.
+    """
+
+    def __init__(self):
+        self._stores: list[QueryEnabledStore] = []
+
+    def store_for(self, socket: DataSocket) -> QueryEnabledStore | None:
+        """
+        Retrieve a store for the provided socket.
+        """
+        matching_store_type = suggest_store(socket)
+        if matching_store_type is None:
+            return None
+
+        for store in self._stores:
+            if isinstance(store, matching_store_type):
+                return store
+        return None
+
+    def load_stores(self, store_connections: list[str]) -> Self:
+        """
+         Populate this collection from the configuration.
+        :return:
+        """
+        self._stores.clear()
+
+        for store_connection in store_connections:
+            self._stores.append(QueryEnabledStore.from_string(store_connection))
+
+        return self
+
+    def close(self) -> None:
+        """
+         Close all stores.
+        :return:
+        """
+        for store in self._stores:
+            store.close()
