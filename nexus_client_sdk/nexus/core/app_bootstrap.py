915fcd1668b384a6eed1cc9df7dca47f4995ebc7
MAPET <marcusforby@gmail.com>
2026-08-26 08:28:12 +0200

    Fix tests

--- nexus_client_sdk/nexus/core/app_bootstrap.py
+++ nexus_client_sdk/nexus/core/app_bootstrap.py
@@ -312,7 +312,11 @@ class NexusBootstrapper:
                 save_content=NEXUS_FRAMEWORK_CONFIGURATION.default.runtime.payload.serialization_mode
                 != _PayloadSerializationMode.OFF.value,
             )
-            app_injector.binder.bind(payload.__class__, to=payload, scope=singleton)
+            # Bind payload to its concrete class and AlgorithmPayload parent classes
+            # so dependencies typed as a base payload class resolve for subclass payloads.
+            for class_type in payload.__class__.mro():
+                if issubclass(class_type, AlgorithmPayload):
+                    app_injector.binder.bind(class_type, to=payload, scope=singleton)
             logger_fixed_template |= self._log_enricher(payload, self._run_args) if self._log_enricher else {}
             logger_tags |= self._log_tagger(payload, self._run_args) if self._log_tagger else {}
             metric_tags |= self._metric_tagger(payload, self._run_args) if self._metric_tagger else {}
