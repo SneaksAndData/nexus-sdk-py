40c3dc5e794a31fd3909dc8af092d898291db795
George Zubrienko <gzu@ecco.com>
2026-08-26 15:28:23 +0200

    WIP

--- nexus_client_sdk/nexus/core/app_bootstrap.py
+++ nexus_client_sdk/nexus/core/app_bootstrap.py
@@ -66,24 +66,6 @@ def _load_data_sockets() -> SocketCollection:
     return base_collection
 
 
-def _load_config_extension(extension_name: str) -> None:
-    if extension_name == "":
-        return
-
-    config_location = os.path.join(
-        os.getenv("CONFIG_EXTENSION_PATH_OVERRIDE", "config_extensions"),
-        "**",
-        f"settings.{extension_name}*.toml",
-    )
-    matching_configurations = [
-        os.path.abspath(conf) for conf in glob.glob(config_location, recursive=True) if os.path.isfile(conf)
-    ]
-    for matching_config in matching_configurations:
-        settings_loader(NEXUS_FRAMEWORK_CONFIGURATION.default, filename=matching_config)
-
-    return
-
-
 class _PayloadSerializationMode(Enum):
     """
     Serialization modes for [runtime.payload.serialization_mode]. Bootstrap-only access.
@@ -244,7 +226,7 @@ class NexusBootstrapper:
             raise FatalStartupConfigurationError(f"Failed to locate a provided algorithm class: {algorithm}")
         self._algorithm_classes.add(algorithm_class)
         # load linked configuration if exists
-        _load_config_extension(algorithm_class.alias())
+        NEXUS_FRAMEWORK_CONFIGURATION.load_config_extension(algorithm_class.alias())
 
     def _load_configured_algorithms(self):
         for algorithm in NEXUS_FRAMEWORK_CONFIGURATION.default.runtime.algorithms:
@@ -287,7 +269,7 @@ class NexusBootstrapper:
         :return:
         """
         self._load_additional_modules()
-        _load_config_extension("provided")
+        NEXUS_FRAMEWORK_CONFIGURATION.load_config_extension("provided")
 
         app_injector = Injector(self._injection_binds)
         self._load_payload_types()
--- tests/nexus/configuration/test_runtime_configuration.py
+++ tests/nexus/configuration/test_runtime_configuration.py
@@ -1,8 +1,105 @@
-from nexus_client_sdk.nexus.configurations.configuration_model import NexusConfigurationModel
+from pathlib import Path
+
+import pytest
+from pydantic.dataclasses import dataclass
+
+from nexus_client_sdk.nexus.configurations.configuration_model import (
+    NexusConfigurationModel,
+    NexusClientSettings,
+    ResultSettings,
+    TelemetrySettings,
+    LoggingSettings,
+    MetricsSettings,
+    RuntimeSettings,
+)
 from nexus_client_sdk.nexus.configurations.runtime_configuration import NEXUS_FRAMEWORK_CONFIGURATION
+
+
+@dataclass
+class CustomSettings:
+    field_a: str
+    field_b: int
+    field_c: bool
+
+
+@dataclass
+class CustomConfigurationModel(NexusConfigurationModel):
+    custom_settings: CustomSettings
+    field_d: float
 
 
 def test_runtime_configuration() -> None:
     NEXUS_FRAMEWORK_CONFIGURATION.load()
     model = NexusConfigurationModel.from_runtime_configuration(NEXUS_FRAMEWORK_CONFIGURATION)
+
     assert model.algorithm_name == "hello-world"
+    assert isinstance(model.client, NexusClientSettings)
+    assert model.client.receiver == "http://localhost:5555/receiver"
+    assert model.client.scheduler == "http://localhost:5555/scheduler"
+
+    assert isinstance(model.result, ResultSettings)
+    assert model.result.storage_client_class == "adapta.storage.blob.s3_storage_client.S3StorageClient"
+
+    assert isinstance(model.telemetry, TelemetrySettings)
+    assert model.telemetry.input.enabled is True
+    assert model.telemetry.user.enabled is True
+
+    assert isinstance(model.logging, LoggingSettings)
+    assert model.logging.datadog.enabled is False
+
+    assert isinstance(model.metrics, MetricsSettings)
+    assert model.metrics.provider == "adapta.metrics.providers.void_provider.VoidMetricsProvider"
+
+    assert isinstance(model.runtime, RuntimeSettings)
+    assert isinstance(model.runtime.algorithms, list)
+
+
+def test_runtime_configuration_nested_models() -> None:
+    NEXUS_FRAMEWORK_CONFIGURATION.load()
+    model = NexusConfigurationModel.from_runtime_configuration(NEXUS_FRAMEWORK_CONFIGURATION)
+
+    assert model.remote_algorithm.dry_run is False
+    assert model.services.query_enabled_store.enabled is False
+    assert isinstance(model.inputs.sockets, list)
+    assert model.inputs.astra_client.enabled == "0"
+    assert model.inputs.trino_client.enabled == "0"
+    assert model.threading.blocking_pool_max_size == "16"
+
+
+@pytest.fixture(autouse=False)
+def set_config_extension_path_override(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setenv("CONFIG_EXTENSION_PATH_OVERRIDE", str(Path(__file__).parent / "config_extensions"))
+
+
+def test_custom_runtime_configuration(set_config_extension_path_override) -> None:
+    NEXUS_FRAMEWORK_CONFIGURATION.load()
+    NEXUS_FRAMEWORK_CONFIGURATION.load_config_extension("provided")
+    model = CustomConfigurationModel.from_runtime_configuration(NEXUS_FRAMEWORK_CONFIGURATION)
+
+    # check custom fields
+    assert model.field_d == 1.2323
+    assert model.custom_settings.field_a == "a"
+    assert model.custom_settings.field_b == 1
+    assert model.custom_settings.field_c is True
+
+    # check inherited settings
+    assert model.algorithm_name == "hello-world"
+    assert isinstance(model.client, NexusClientSettings)
+    assert model.client.receiver == "http://localhost:5555/receiver"
+    assert model.client.scheduler == "http://localhost:5555/scheduler"
+
+    assert isinstance(model.result, ResultSettings)
+    assert model.result.storage_client_class == "adapta.storage.blob.s3_storage_client.S3StorageClient"
+
+    assert isinstance(model.telemetry, TelemetrySettings)
+    assert model.telemetry.input.enabled is True
+    assert model.telemetry.user.enabled is True
+
+    assert isinstance(model.logging, LoggingSettings)
+    assert model.logging.datadog.enabled is False
+
+    assert isinstance(model.metrics, MetricsSettings)
+    assert model.metrics.provider == "adapta.metrics.providers.void_provider.VoidMetricsProvider"
+
+    assert isinstance(model.runtime, RuntimeSettings)
+    assert isinstance(model.runtime.algorithms, list)
