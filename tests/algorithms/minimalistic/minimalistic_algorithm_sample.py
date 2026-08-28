4b9db502e9c642f557d5efade2ab6d16357bdc8b
George Zubrienko <gzu@ecco.com>
2026-08-28 10:24:22 +0200

    fix tests

--- tests/algorithms/minimalistic/minimalistic_algorithm_sample.py
+++ tests/algorithms/minimalistic/minimalistic_algorithm_sample.py
@@ -29,8 +29,17 @@ class TestMinimalisticAlgorithm(MinimalisticAlgorithm[TestAlgorithmPayload, Test
         z_processor: ZProcessor,
         zz_processor: ZZProcessor,
         cache: InputCache,
+        configuration_model: TestAlgorithmConfiguration,
     ):
-        super().__init__(metrics_provider, logger_factory, xy_processor, z_processor, zz_processor, cache=cache)
+        super().__init__(
+            metrics_provider,
+            logger_factory,
+            xy_processor,
+            z_processor,
+            zz_processor,
+            cache=cache,
+            configuration_model=configuration_model,
+        )
 
     async def _run(self, xy: pandas.DataFrame, z: pandas.DataFrame, zz: pandas.DataFrame, **kwargs) -> TestResult:
         assert (
