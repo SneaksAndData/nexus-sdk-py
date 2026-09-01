831b7533491f9940456a73d5f7a49e19f87b4c02
MAPET <marcusforby@gmail.com>
2026-09-01 13:55:31 +0200

    BUG-FIX: Make sure QES Collection Factory loads stores

--- nexus_client_sdk/nexus/core/app_dependencies.py
+++ nexus_client_sdk/nexus/core/app_dependencies.py
@@ -48,7 +48,7 @@ class QueryEnabledStoreCollectionFactory:
         """
         DI factory method.
         """
-        if not model.services.query_enabled_store:
+        if model.services.query_enabled_store.enabled:
             return QueryEnabledStoreCollection()
 
         try:
